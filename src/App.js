d074ef75ad1d5299696b17e03e98a8d1c4d67ad2
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-10-01 15:06:22 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
 import "./styles.css";
 
-function Animal({ emoji, name }) {
+function Animal({ emoji, name, description }) {
   return (
-    <h2>
-      {emoji} {name}
-    </h2>
+    <>
+      <h2>
+        {emoji} {name}
+      </h2>
+      {description}
+      <Button>
+        Adopt <strong>{name}</strong>
+      </Button>
+    </>
   );
 }
 
@@ -14,10 +19,31 @@ export default function App() {
     <main>
       <h1>Animal Shelter</h1>
       <p>Please adopt our lovely pets!</p>
-      <Animal emoji="🐕" name="Lucky" />
-      <Animal emoji="🐈" name="Mittens" />
-      <Animal emoji="🐢" name="Archibald" />
+      <Animal emoji="🐕" name="Lucky" description={<p>Very cute dog</p>} />
+      <Animal emoji="🐈" name="Mittens" description={<p>Very nice kitty</p>} />
+      <Animal
+        emoji="🐢"
+        name="Archibald"
+        description={
+          <>
+            <p>Super relaxed turtle</p>
+            <ul>
+              <li>Eats lettuce all the time</li>
+              <li>Can sleep in your fridge</li>
+            </ul>
+          </>
+        }
+      />
       <Animal emoji="🐣" name="Chick Norris" />
     </main>
+  );
+}
+
+function Button({ children }) {
+  console.log("Children: ", children);
+  return (
+    <button type="button" className="button">
+      {children}
+    </button>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <App /> 
   </StrictMode>
 );
